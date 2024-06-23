369f62101ca8f3c4424f915dc2005c7c9b4a3630
Sarathamyres <thamyressara071@gmail.com>
2024-06-23 02:28:35 -0300

    fiz alguns ajustes. Diria que o projeto está uns 90% finalizado talvez

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-const hamburger = document.querySelector('.icon-bars');
-const cancel = document.querySelector('.div-x-menu-hamburger');
-const listHamburger = document.querySelector('.list-hamburger');
-const categoryLabel = document.querySelector('.category');
-const checkbox = document.getElementById('category')
+const hamburger = document.querySelector(".icon-bars");
+const cancel = document.querySelector(".div-x-menu-hamburger");
+const listHamburger = document.querySelector(".list-hamburger");
+const categoryLabel = document.querySelector(".category");
+const checkbox = document.getElementById("category");
+const iconFavorite = document.querySelector(".heart");
+const iconSolid = document.querySelector(".solid-heart");
+const iconFavorites = document.querySelectorAll(".heart");
+const iconSolids = document.querySelectorAll(".solid-heart");
 
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.add('hidden');
-  if(hamburger.classList.contains('hidden')){
-    cancel.style.display = 'block';
-    listHamburger.style.display = 'block'
+//menu-hamburger
+hamburger.addEventListener("click", () => {
+  hamburger.classList.add("hidden");
+  if (hamburger.classList.contains("hidden")) {
+    cancel.style.display = "block";
+    listHamburger.style.display = "block";
   }
-  cancel.addEventListener('click', () => {
-    cancel.style.display = 'none';
-    if(cancel.style.display === 'none'){
-      hamburger.classList.remove('hidden');
-      listHamburger.style.display = 'none'
+  cancel.addEventListener("click", () => {
+    cancel.style.display = "none";
+    if (cancel.style.display === "none") {
+      hamburger.classList.remove("hidden");
+      listHamburger.style.display = "none";
     }
-  })
+  });
 });
 
-checkbox.addEventListener('change', () => {
- if(checkbox.checked){
-  categoryLabel.classList.add('color-category');
-} else {
-  categoryLabel.classList.remove('color-category'); 
-}
-})
+//box-category
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    categoryLabel.classList.add("color-category");
+  } else {
+    categoryLabel.classList.remove("color-category");
+  }
+});
+
+//icons-favorite
+iconFavorites.forEach(iconFavorite => {
+  iconFavorite.addEventListener("click", () => {
+    iconFavorites.forEach(icon => icon.classList.add("hidden"));
+    iconSolids.forEach(icon => icon.classList.remove("hidden"));
+  });
+});
+
+iconSolids.forEach(iconSolid => {
+  iconSolid.addEventListener("click", () => {
+    iconSolids.forEach(icon => icon.classList.add("hidden"));
+    iconFavorites.forEach(icon => icon.classList.remove("hidden"));
+  });
+});
