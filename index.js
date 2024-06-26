2a04f509d69f47bde8af3bbf41350389dd297700
Sarathamyres <thamyressara071@gmail.com>
2024-06-26 16:31:04 -0300

    potencialmente finalizado

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const iconFavorite = document.querySelector(".heart");
 const iconSolid = document.querySelector(".solid-heart");
 const iconFavorites = document.querySelectorAll(".heart");
 const iconSolids = document.querySelectorAll(".solid-heart");
+const arrows = document.querySelectorAll(".arrow");
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselItems = document.querySelectorAll(".item-img");
+
+const carouselContainertwo = document.querySelector('.carousel-container2');
+const arrowstwo = document.querySelectorAll(".arrow2");
+
+// Defina a largura do primeiro item do carrossel após carregar a página
+window.addEventListener('load', () => {
+  const firstCardWidth = carouselItems[0].clientWidth + 20; // 20px é o gap entre os itens
+
+  arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      if (arrow.id === "left") {
+        carouselContainer.scrollLeft -= firstCardWidth;
+      } else {
+        carouselContainer.scrollLeft += firstCardWidth;
+      }
+    });
+  });
+});
+
+window.addEventListener('load', () => {
+  const firstCardWidth = carouselItems[0].clientWidth + 20; // 20px é o gap entre os itens
+
+  arrowstwo.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+      if (arrow.id === "left2") {
+        carouselContainertwo.scrollLeft -= firstCardWidth;
+      } else {
+        carouselContainertwo.scrollLeft += firstCardWidth;
+      }
+    });
+  });
+});
 
 //menu-hamburger
 hamburger.addEventListener("click", () => {
@@ -34,16 +69,16 @@ checkbox.addEventListener("change", () => {
 });
 
 //icons-favorite
-iconFavorites.forEach(iconFavorite => {
+iconFavorites.forEach((iconFavorite) => {
   iconFavorite.addEventListener("click", () => {
-    iconFavorites.forEach(icon => icon.classList.add("hidden"));
-    iconSolids.forEach(icon => icon.classList.remove("hidden"));
+    iconFavorites.forEach((icon) => icon.classList.add("hidden"));
+    iconSolids.forEach((icon) => icon.classList.remove("hidden"));
   });
 });
 
-iconSolids.forEach(iconSolid => {
+iconSolids.forEach((iconSolid) => {
   iconSolid.addEventListener("click", () => {
-    iconSolids.forEach(icon => icon.classList.add("hidden"));
-    iconFavorites.forEach(icon => icon.classList.remove("hidden"));
+    iconSolids.forEach((icon) => icon.classList.add("hidden"));
+    iconFavorites.forEach((icon) => icon.classList.remove("hidden"));
   });
 });
